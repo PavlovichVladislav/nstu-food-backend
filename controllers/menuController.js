75bd121abce2d4eee7a97ec4825ddbdeefbe602b
PavlovichVladislav <jeffry2814@gmail.com>
2023-05-07 21:46:25 +0700

    finished menuController

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -3,23 +3,35 @@ const { MenuItem, Menu, Dish } = require("../models/models");
 class MenuController {
    async getRestuarantMenu(req, res) {
       const { restuarantId } = req.params;
+      let menu = null;
 
       if (restuarantId) {
-         const menu = await Menu.findOne({where: { restuarantId }});
-         const menuId = menu.id;
-         const menuItems = await MenuItem.findAll({where: {menuId}});
-
-         return res.json(menuItems);
+         menu = await Menu.findOne({ where: { restuarantId } });
       }
+
+      if (menu) {
+         const restuarantDishes = [];
+         const menuItems = await MenuItem.findAll({ where: { menuId: menu.id } });
+
+         for (let menuItem of menuItems) {
+            const dish = await Dish.findOne({ where: { id: menuItem.dataValues.dishId } });
+
+            restuarantDishes.push(dish);
+         }
+
+         return res.json(restuarantDishes);
+      }
+
+      return res.json([]);
    }
 
    async create(req, res) {
       const { dishId, menuId } = req.body;
 
-      const menuItem = await MenuItem.create({dishId, menuId});
+      const menuItem = await MenuItem.create({ dishId, menuId });
 
       return res.json(menuItem);
-   } 
+   }
 }
 
 module.exports = new MenuController();
--- routes/menuRouter.js
+++ routes/menuRouter.js
@@ -2,8 +2,6 @@ const Router = require('express');
 const router = new Router();
 const menuController = require('../controllers/menuController');
 
-// получить меню для конкретной столовой
-
 router.get('/:restuarantId', menuController.getRestuarantMenu);
 router.post('/', menuController.create);
 
