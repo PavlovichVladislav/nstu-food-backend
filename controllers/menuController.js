ef87b60fda4ece891fa5f8a21fabe0b9957aa17d
PavlovichVladislav <jeffry2814@gmail.com>
2023-05-10 11:33:46 +0700

    added output of name of rest with the menu

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -1,28 +1,33 @@
-const { MenuItem, Menu, Dish } = require("../models/models");
+const { MenuItem, Menu, Dish, Restuarant } = require("../models/models");
 
 class MenuController {
    async getRestuarantMenu(req, res) {
       const { restuarantId } = req.params;
       let menu = null;
+      let restuarantName = "";
+      const dishes = [];
 
       if (restuarantId) {
          menu = await Menu.findOne({ where: { restuarantId } });
+         const rest = await Restuarant.findOne({ where: { id: restuarantId } });
+
+         if (rest) restuarantName = rest.toJSON().name;
       }
 
       if (menu) {
-         const restuarantDishes = [];
          const menuItems = await MenuItem.findAll({ where: { menuId: menu.id } });
 
          for (let menuItem of menuItems) {
-            const dish = await Dish.findOne({ where: { id: menuItem.dataValues.dishId } });
+            const dish = await Dish.findOne({ where: { id: menuItem.toJSON().dishId } });
 
-            restuarantDishes.push(dish);
+            dishes.push(dish);
          }
-
-         return res.json(restuarantDishes);
       }
 
-      return res.json([]);
+      return res.json({
+         restuarantName,
+         dishes,
+      });
    }
 
    async create(req, res) {
